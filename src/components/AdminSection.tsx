efa65cc150189cd752a97e749ffad75fcded17e9
Jayapal Reddy <saijoshna@Vasudeva.local>
2026-06-17 02:27:38 +0530

    fix: replace react-quill with react-quill-new to fix React 19 crash

--- src/components/AdminSection.tsx
+++ src/components/AdminSection.tsx
@@ -7,8 +7,8 @@ import { CafeForm } from './CafeForm';
 import Papa from 'papaparse';
 
 // @ts-ignore
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 
 interface AdminSectionProps {
   cafes: Cafe[];
