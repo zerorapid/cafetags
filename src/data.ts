88458ed9e8eaee283948506fc07d098645c16ede
Jayapal Reddy <saijoshna@Vasudeva.local>
2026-06-27 00:49:07 +0530

    Update La Vie En Rose Cafe images

--- src/data.ts
+++ src/data.ts
@@ -625,54 +625,33 @@ export const INITIAL_CAFES: Cafe[] = [
     "takeaway": false,
     "onlineOrder": false,
     "mapLink": "https://maps.google.com/?cid=8569201108133998912&g_mp=Cidnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLlNlYXJjaFRleHQ",
-    "image": "https://b.zmtcdn.com/data/pictures/9/21365469/cd381a727e56394dc4f18042fc2f0ef8.jpg",
+    "image": "https://lh3.googleusercontent.com/0pMiuZ8KFjDjyZX1_neMajnBwYMkRlW1eWe61cq8-thJhshaLyG3XSKCCSyE6eMyKVOQ0CV89Ze7GISi3T4iHZSLuHbRCcvdYsX2gbA=w1200-rw",
     "logo": "https://b.zmtcdn.com/data/brand_creatives/logos/d4446b7aa9e756d7d859111f2aeaf11b1746435804.png",
     "moreImages": [
-      "https://b.zmtcdn.com/data/pictures/9/21365469/35c912438a451a4ed875cc22470199dc.jpg?crop=700%3A400%3B%2A%2C%2A&fit=around%7C700%3A400&output-format=webp",
-      "https://b.zmtcdn.com/data/pictures/9/21365469/1a14df324279a971bc7bb891b194819a.png",
-      "https://b.zmtcdn.com/data/pictures/chains/9/21365469/67b427acd08af1de7c8d166716732488.jpg",
-      "https://b.zmtcdn.com/data/pictures/9/21365469/cd381a727e56394dc4f18042fc2f0ef8.jpg",
-      "https://b.zmtcdn.com/data/pictures/chains/9/21365469/67b427acd08af1de7c8d166716732488.jpg?crop=700%3A400%3B%2A%2C%2A&fit=around%7C700%3A400&output-format=webp"
+      "https://lh3.googleusercontent.com/B3nSK_lJl2qswmYK0ucfOVqsHoy9IMRHR3EhXEkB5Kl2Jsg5wgDzwjXbii23ARVf33_2lfAard74zGIdnd8OQ62vBB88w8dXtfNeSfQ=w1200-rw",
+      "https://lh3.googleusercontent.com/giXgqSCG8mCiPQs6rur5FjtsT5bQgQaDpvSUQzfNAkzKbHj3F2dE5fpDpGxDD228h4DS0APFGeRoH0lH04ZjQ9fcUPA=w1200-rw",
+      "https://lh3.googleusercontent.com/vw9uqd2j2GvMnkLIEV_bdN4iq-n3tCfpQHm9y-r_6Pwm9a4g78-NMOz1go4eeuZ7sD2hDCU7VDdNLlk76i82zfR8jjXoNfUSc0bp8DZ0=w1200-rw",
+      "https://lh3.googleusercontent.com/F-ApgpHy98KpeCQq9o5R4om3XhakHHNmfOpyW0ur-ejWwBZ4eaCkvyVPnuKbpGNVSckFcz4cdRznYZ2-Ca3kTr_dcEE=w1200-rw"
     ],
-    "menuImages": [
-      "https://b.zmtcdn.com/data/menus/469/21365469/8d695c86299ee6781bfaec07467bf0c1.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/154faf08746c5d81b45e20bd5931383a.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/f1bd9d9db16a6dd34f21880708ad98b8.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/5f11051259262b30216bb991e758f31f.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/79955272bf689f9ac1cf7ad8bfa6a716.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/21e56cac02e81358b8b1b9feb4bee5b7.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/bd88815cffbac7eac7f786bc4f9e18ba.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/6b01d0a9802239484faab04fa55b78ed.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/7b5e579da9d22f8855b2f740e58a5887.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/ae463e59ec4fc6cb4ce197e104a57bc9.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/3bab54b3e3d638aa13bc5d75c5b94b76.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/ca7c4c5199a6b782cc3c1148112ab766.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/6fb3b73064bd2aafa1fc3eed41dd235a.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/4c15ce800a90e54154885f59244a8500.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/42cb88b061de38e62375becc1837ae40.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/cd8aded324cb78e2a8c99efafb2a0979.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/e60c4b04b6541e0ea13c9387f9bcacf4.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/865068f9276f2c229674093bcb4177cd.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/8c0315eae5f3b14b0e651f922c447127.png"
-    ],
+    "menuImages": [],
     "featuredMenu": [
       {
         "name": "Rose Tea",
         "price": "₹350",
         "description": "Signature featured item",
-        "image": "https://b.zmtcdn.com/data/pictures/chains/9/21365469/0435a4e9762114b39061b77cffb50eb1.jpg?crop=700%3A400%3B%2A%2C%2A&fit=around%7C700%3A400&output-format=webp"
+        "image": "https://lh3.googleusercontent.com/1IelHD7xOcQDxhGV1fyfK7kygHojE0ZGWLE7GurSArhkRL5Qnk2mYEZPcuiCNAH5AJExVGOER0aypybtqtDC2Prwpu31iUnF0CVxsr5g=w1200-rw"
       },
       {
         "name": "Pink Crepe",
         "price": "₹320",
         "description": "Delicious signature creation",
-        "image": "https://b.zmtcdn.com/data/pictures/chains/9/21365469/67b427acd08af1de7c8d166716732488.jpg?crop=700%3A400%3B%2A%2C%2A&fit=around%7C700%3A400&output-format=webp"
+        "image": "https://lh3.googleusercontent.com/c9wukWuKg2eG29pLRUJ6sL5vOYePU5OSk-z6N5V1_Gr5XMyqJEiQO3U3l7G40TzrSXqboXOIdqZ6IdXRKp0Sj7DPOE_rMzTO0m1HQCw=w1200-rw"
       },
       {
         "name": "Floral Pastry",
         "price": "₹280",
         "description": "Classic premium selection",
-        "image": "https://b.zmtcdn.com/data/reviews_photos/d27/0208b3d53dfeef35c44e6a9e95426d27_1777992138.jpg"
+        "image": "https://lh3.googleusercontent.com/cZzbb-_2XB1gQly1XFqZKy-jO5DEtLf1tPqz3S8T9ANL61FecU2QL8nU7mkLJFT7BWRu2Zl4BHiKoFsVbHpc_2LnC6xZtOJST2J0aDGzcQ=w1200-rw"
       }
     ],
     "email": "",
