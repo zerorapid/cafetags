2b65b81fb6dc08627dddfc593dd8cfd698a0a2dc
Jayapal Reddy <saijoshna@Vasudeva.local>
2026-06-27 02:30:57 +0530

    Update Makobrew Cafe with real extracted District images

--- src/data.ts
+++ src/data.ts
@@ -964,54 +964,45 @@ export const INITIAL_CAFES: Cafe[] = [
     "takeaway": true,
     "onlineOrder": true,
     "mapLink": "https://maps.google.com/?cid=4892250763931573143&g_mp=Cidnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLlNlYXJjaFRleHQ",
-    "image": "https://b.zmtcdn.com/data/pictures/9/21365469/cd381a727e56394dc4f18042fc2f0ef8.jpg",
+    "image": "https://b.zmtcdn.com/data/pictures/2/19258392/c8b21115bb9919b4563a34a8e60249e0.jpeg",
     "logo": "https://b.zmtcdn.com/data/brand_creatives/logos/d4446b7aa9e756d7d859111f2aeaf11b1746435804.png",
     "moreImages": [
-      "https://b.zmtcdn.com/data/pictures/9/21365469/35c912438a451a4ed875cc22470199dc.jpg?crop=700%3A400%3B%2A%2C%2A&fit=around%7C700%3A400&output-format=webp",
-      "https://b.zmtcdn.com/data/pictures/9/21365469/1a14df324279a971bc7bb891b194819a.png",
-      "https://b.zmtcdn.com/data/pictures/chains/9/21365469/67b427acd08af1de7c8d166716732488.jpg",
-      "https://b.zmtcdn.com/data/pictures/9/21365469/cd381a727e56394dc4f18042fc2f0ef8.jpg",
-      "https://b.zmtcdn.com/data/pictures/chains/9/21365469/67b427acd08af1de7c8d166716732488.jpg?crop=700%3A400%3B%2A%2C%2A&fit=around%7C700%3A400&output-format=webp"
+      "https://b.zmtcdn.com/data/pictures/chains/2/19258392/1f3910c5da800ed91b65e900c3b0eb41.jpeg",
+      "https://b.zmtcdn.com/data/pictures/2/19258392/1a0b332308eb675c5f87b8f041793617.jpeg",
+      "https://b.zmtcdn.com/data/pictures/chains/2/19258392/0bdf60f7f329976378e9a26322b74dd6.jpeg",
+      "https://b.zmtcdn.com/data/pictures/2/19258392/37fcde16c1471de9996d1945f342cdfa.jpeg"
     ],
     "menuImages": [
-      "https://b.zmtcdn.com/data/menus/469/21365469/8d695c86299ee6781bfaec07467bf0c1.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/154faf08746c5d81b45e20bd5931383a.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/f1bd9d9db16a6dd34f21880708ad98b8.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/5f11051259262b30216bb991e758f31f.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/79955272bf689f9ac1cf7ad8bfa6a716.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/21e56cac02e81358b8b1b9feb4bee5b7.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/bd88815cffbac7eac7f786bc4f9e18ba.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/6b01d0a9802239484faab04fa55b78ed.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/7b5e579da9d22f8855b2f740e58a5887.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/ae463e59ec4fc6cb4ce197e104a57bc9.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/3bab54b3e3d638aa13bc5d75c5b94b76.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/ca7c4c5199a6b782cc3c1148112ab766.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/6fb3b73064bd2aafa1fc3eed41dd235a.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/4c15ce800a90e54154885f59244a8500.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/42cb88b061de38e62375becc1837ae40.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/cd8aded324cb78e2a8c99efafb2a0979.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/e60c4b04b6541e0ea13c9387f9bcacf4.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/865068f9276f2c229674093bcb4177cd.png",
-      "https://b.zmtcdn.com/data/menus/469/21365469/8c0315eae5f3b14b0e651f922c447127.png"
+      "https://b.zmtcdn.com/data/menus/392/19258392/3fcff63896a8edace12019c6e67fe46e.jpeg",
+      "https://b.zmtcdn.com/data/menus/392/19258392/baabbdf1c4a32006e8b002e7b54e32a6.jpeg",
+      "https://b.zmtcdn.com/data/menus/392/19258392/8b633a6d05b613b3fa322d697610894f.jpeg",
+      "https://b.zmtcdn.com/data/menus/392/19258392/aea8619e9100c4afeae697219dcfe37b.jpeg",
+      "https://b.zmtcdn.com/data/menus/392/19258392/742653f4e0252a0e8319622233fe486c.jpeg",
+      "https://b.zmtcdn.com/data/menus/392/19258392/80f31a2a2f154572f295b5593b756278.jpeg",
+      "https://b.zmtcdn.com/data/menus/392/19258392/d01405795d5dcdc330311d450a66f4cc.jpeg",
+      "https://b.zmtcdn.com/data/menus/392/19258392/4fc9f6bb41f42919fad6cdfef72b553f.jpeg",
+      "https://b.zmtcdn.com/data/menus/392/19258392/d5606abe6cdca6aa03a1a192ddc8c36e.jpeg",
+      "https://b.zmtcdn.com/data/menus/392/19258392/0609ad74417187773e7f70c97d0bf3b8.jpeg",
+      "https://b.zmtcdn.com/data/menus/392/19258392/dbd11bbaf8883137e8a80ff9c784c54b.jpeg"
     ],
     "featuredMenu": [
       {
         "name": "Sweet Crepe",
         "price": "₹350",
         "description": "Signature sweet crepe dessert",
-        "image": "https://b.zmtcdn.com/data/pictures/chains/9/21365469/0435a4e9762114b39061b77cffb50eb1.jpg?crop=700%3A400%3B%2A%2C%2A&fit=around%7C700%3A400&output-format=webp"
+        "image": "https://b.zmtcdn.com/data/pictures/chains/2/19258392/1e93836dc2a7a4beaeec1541b9dcd87b.jpeg"
       },
       {
         "name": "Beetroot Burger",
         "price": "₹320",
         "description": "Gourmet vegetarian burger with beetroot patty",
-        "image": "https://b.zmtcdn.com/data/pictures/chains/9/21365469/67b427acd08af1de7c8d166716732488.jpg?crop=700%3A400%3B%2A%2C%2A&fit=around%7C700%3A400&output-format=webp"
+        "image": "https://b.zmtcdn.com/data/pictures/2/19258392/d40d9904d9c490a6f8749d0124ca6032.jpeg"
       },
       {
         "name": "Cheesecake",
         "price": "₹280",
         "description": "Classic creamy cheesecake slice",
-        "image": "https://b.zmtcdn.com/data/reviews_photos/d27/0208b3d53dfeef35c44e6a9e95426d27_1777992138.jpg"
+        "image": "https://b.zmtcdn.com/data/pictures/chains/2/19258392/f0e635f79576cf1a7c06eb3c4ab5f257.jpeg"
       }
     ],
     "email": "",
